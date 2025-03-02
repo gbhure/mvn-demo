7e3a38186aac324d7172865abd3bf1cb6051a8f5
Ganesh Bhure <ganesh.bhure@gmail.com>
2025-03-02 21:48:43 +0530

    Update App.java

--- src/main/java/org/company/javaProj/App.java
+++ src/main/java/org/company/javaProj/App.java
@@ -5,6 +5,6 @@ package org.company.javaProj;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World123!");
+        System.out.println("Hello World123 567!");
     }
 }
